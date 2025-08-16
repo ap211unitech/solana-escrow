f7f51b1f112844137b9e26cff0447a77f13754eb
Arjun Porwal <porwalarjun95@gmail.com>
2025-08-16 13:10:24 +0530

    feat: initialize escrow app

---
+++ programs/escrow-app/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("5gdV4b4cPnnRkVSvBq8WxCxRfyq7i5z9R5scwm3BA4ps");
+
+#[program]
+pub mod escrow_app {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
---
+++ tests/src/test_initialize.rs
@@ -0,0 +1,28 @@
+use std::str::FromStr;
+
+use anchor_client::{
+    solana_sdk::{
+        commitment_config::CommitmentConfig, pubkey::Pubkey, signature::read_keypair_file,
+    },
+    Client, Cluster,
+};
+
+#[test]
+fn test_initialize() {
+    let program_id = "5gdV4b4cPnnRkVSvBq8WxCxRfyq7i5z9R5scwm3BA4ps";
+    let anchor_wallet = std::env::var("ANCHOR_WALLET").unwrap();
+    let payer = read_keypair_file(&anchor_wallet).unwrap();
+
+    let client = Client::new_with_options(Cluster::Localnet, &payer, CommitmentConfig::confirmed());
+    let program_id = Pubkey::from_str(program_id).unwrap();
+    let program = client.program(program_id).unwrap();
+
+    let tx = program
+        .request()
+        .accounts(escrow_app::accounts::Initialize {})
+        .args(escrow_app::instruction::Initialize {})
+        .send()
+        .expect("");
+
+    println!("Your transaction signature {}", tx);
+}
